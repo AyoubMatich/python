45375f17a0341e5e960bbbdd252c559a3d6d195c
AyoubMatich <matichayyoub88@gmail.com>
2023-09-12 23:51:33 +0100

    add a try except

--- python_0/calculator.py
+++ python_0/calculator.py
@@ -8,10 +8,19 @@ def div(a, b):
 	return a / b
 def powr(a, b):
 	return pow(a,  b)
-
-nam_1 = float(input("plesse intre frste namber: "))
-oprator = input("enter oprator: ")
-nam_2 = float(input("plesse intre secnde namber: "))
+try:
+	nam_1 = float(input("plesse intre frste namber: "))
+except ValueError as err_1:
+	print(err_1)
+try:
+	oprator = input("enter oprator: ")
+except ValueError as err_2:
+	print(err_2)
+try:
+	nam_2 = float(input("plesse intre secnde namber: "))
+except ValueError as err_3:
+	print(err_3)
+print("resolte : ")
 
 if (oprator == '+' ):
 	print(plas(nam_1, nam_2))
