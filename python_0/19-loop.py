07041147c6012c16045af1a052f82db76249439c
AyoubMatich <matichayyoub88@gmail.com>
2023-09-14 13:15:16 +0100

    add a loop for

--- python_0/19-loop.py
+++ python_0/19-loop.py
@@ -1,5 +1,11 @@
-i=1
-while i<= 5:
-	print("i= ", i)
-	i+= 1
+#i=1
+#while i<= 5:
+#	print("i= ", i)
+#	i+= 1
 
+piple = ["ali", "same","laila", "gara"]
+star = [1,2,3,4,5,6,7,8,9]
+for row in piple:
+	print(row)
+for st in star:
+	print(st)
